93c4a25b4c58b086a0321622847b0223de7a912c
OSS0302 <tkdtn3264@gmail.com>
2024-01-10 22:40:34 +0900

    뷰모델 작성

---
+++ lib/presentation/main/main_veiw_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:wather_app/data/data_source/weather_api.dart';
+import 'package:wather_app/data/repository/weather_repository_info_impl.dart';
+import 'package:wather_app/domain/model/weather_info.dart';
+
+class MainViewModel extends ChangeNotifier {
+  final WeatherApi  _api = WeatherApi();
+  late final WeatherRepositoryImpl impl;
+   WeatherInfo?  weatherData;
+
+  Future<void> getRead(num lat , num long) async{
+    impl = WeatherRepositoryImpl(api:_api);
+
+    final result = await impl.getWeatherInfo(lat,long);
+    weatherData = result;
+  }
+}
